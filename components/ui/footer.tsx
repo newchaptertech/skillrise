4e4c64e58050e67ee5f016fdf98bb46bbfcba21d
xue <behicsakar@gmail.com>
2025-07-15 23:38:24 -0400

    Remove social media links from Footer component to streamline design and focus on essential content.

--- components/ui/footer.tsx
+++ components/ui/footer.tsx
@@ -96,42 +96,6 @@ export default function Footer({ border = false }: { border?: boolean }) {
             <h3 className="text-sm font-medium">Social</h3>
             <ul className="flex gap-1">
               <li>
-                <Link
-                  className="flex items-center justify-center transition"
-                  style={{ color: "#1da1f2" }}
-                  href="#0"
-                  aria-label="Twitter"
-                  onMouseOver={(e) => (e.currentTarget.style.color = "#0d8ddb")}
-                  onMouseOut={(e) => (e.currentTarget.style.color = "#1da1f2")}
-                >
-                  <svg
-                    className="h-8 w-8 fill-current"
-                    viewBox="0 0 32 32"
-                    xmlns="http://www.w3.org/2000/svg"
-                  >
-                    <path d="m13.063 9 3.495 4.475L20.601 9h2.454l-5.359 5.931L24 23h-4.938l-3.866-4.893L10.771 23H8.316l5.735-6.342L8 9h5.063Zm-.74 1.347h-1.457l8.875 11.232h1.36l-8.778-11.232Z"></path>
-                  </svg>
-                </Link>
-              </li>
-              <li>
-                <Link
-                  className="flex items-center justify-center transition"
-                  style={{ color: "#12100e" }}
-                  href="#0"
-                  aria-label="Medium"
-                  onMouseOver={(e) => (e.currentTarget.style.color = "#333")}
-                  onMouseOut={(e) => (e.currentTarget.style.color = "#12100e")}
-                >
-                  <svg
-                    className="h-8 w-8 fill-current"
-                    viewBox="0 0 32 32"
-                    xmlns="http://www.w3.org/2000/svg"
-                  >
-                    <path d="M23 8H9a1 1 0 0 0-1 1v14a1 1 0 0 0 1 1h14a1 1 0 0 0 1-1V9a1 1 0 0 0-1-1Zm-1.708 3.791-.858.823a.251.251 0 0 0-.1.241V18.9a.251.251 0 0 0 .1.241l.838.823v.181h-4.215v-.181l.868-.843c.085-.085.085-.11.085-.241v-4.887l-2.41 6.131h-.329l-2.81-6.13V18.1a.567.567 0 0 0 .156.472l1.129 1.37v.181h-3.2v-.181l1.129-1.37a.547.547 0 0 0 .146-.472v-4.749a.416.416 0 0 0-.138-.351l-1-1.209v-.181H13.8l2.4 5.283 2.122-5.283h2.971l-.001.181Z"></path>
-                  </svg>
-                </Link>
-              </li>
-              <li>
                 <a
                   className="flex items-center justify-center transition"
                   style={{ color: "#333" }}
